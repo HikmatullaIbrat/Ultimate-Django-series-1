8209d89fe5392ae5a36c462eace6bdddabec0cbb
unknown <--global>
2022-12-23 19:13:12 +0430

    default values for contentType and object_id in tags model added

--- tags/models.py
+++ tags/models.py
@@ -13,14 +13,14 @@ class TaggedItem(models.Model):
 
     # we could add a foreignKey of product class to taggetItem class for tagging the items, but it makes 
     # taggedItem dependent to just product class and if want to tag some other contents like videos or 
-    # article we this taggedItem class won't work for them, Therefore we use the built-in contenttype class
+    # article, this taggedItem class won't work for them. Therefore we use the built-in contenttype class
     # so we comment the product foreign key
     # product = models.ForeignKey(Product)
-    # so to use conttype class we have to identify the type and id of the object
+    # so to use contentType class we have to identify the type and id of the object
     # using the type we can find the table and using the id we can find the record
     # for using the contenttpye we have to first import it
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,default=0)
+    object_id = models.PositiveIntegerField(default=0.0)
     content_object = GenericForeignKey()
 
 
